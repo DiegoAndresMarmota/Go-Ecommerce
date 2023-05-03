f9d0f97384071fbba868e319ff985f8768a1f3e3
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-02 20:28:40 -0400

    Features/handler user 01 (#4)
    * Creación del handler (User) con Echo
    
    * Creación del handler (GetAll) de User
    
    * $ go get github.com/labstack/echo/v4

---
+++ infrastructure/handler/user/handler.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"e-commerce/domain/user"
+	"e-commerce/model"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type handler struct {
+	useCase user.UserCase
+}
+
+// NewHandler retorna la implementación del dominio como un punto de entrada   
+func newHandler(uc user.UserCase) handler {
+	return handler{useCase: uc}
+}
+
+//Crea la conexión con Echo
+func (h *handler) Create(c echo.Context) (err error) {
+	m := model.User{}
+
+	//Si model.User == error, informar al cliente de la petición erronea con un .JSON del error
+	if err := c.Bind(&m); err != nil {
+		c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, m)
+}
+
+//Obtiene la información completa del userCase
+func (h *handler) GetAll(c echo.Context) (err error) {
+	users, err := h.useCase.GetAll()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, users)
+}
