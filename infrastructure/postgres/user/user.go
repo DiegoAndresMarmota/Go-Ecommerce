56c548312e8c7abde0217f2862ea4917b6cf79e0
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-04 14:27:48 -0400

    Implementación de Postgres con Pgx

---
+++ infrastructure/postgres/user/user.go
@@ -0,0 +1,81 @@
+package user
+
+import (
+	"context"
+	"e-commerce/model"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+//Declaración de Crear Usuario y Obtener lista de usuario.
+var (
+	psqlInsert = "INSERT INTO users (id, email, password, details, created_at) VALUES ($1, $2, $3, $4, $5)"
+	psqlGetAll = "SELECT id, email, password, details, created_at, updated_at FROM users"
+)
+
+type User struct {
+	db *pgxpool.Pool
+}
+
+//Creación del CRUD con pxlpool en User
+func New(db *pgxpool.Pool) *User {
+	return &User{db: db}
+}
+
+//Creación de User a través de psqlInsert en el model.User
+func (u *User) Create(m *model.User) error {
+	_, err := u.db.Exec(
+		context.Background(),
+		psqlInsert,
+		m.ID,
+		m.Email,
+		m.Password,
+		m.IsAdmin,
+		m.Details,
+		m.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//Obtención de Users a través de psqlSelect en los []model.User, añadiendo Where email
+func (u User) GetByEmail(email string) (model.User, error) {
+	query := psqlGetAll + "WHERE email = $1"
+	row := u.db.QueryRow(
+		context.Background(),
+		query,
+		email,
+	)
+	return u.scanRow(row)
+}
+
+//
+func (u User) GetAll() (model.Users, error) {
+	rows, err := u.db.Query(
+		context.Background(),
+		psqlGetAll,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	ms:= model.Users{}
+	for rows.Next() {
+		m, err := u.scanRow(rows)
+		if err != nil {
+			return nil, err
+		}
+		ms = append(ms, m)
+	}
+	return ms, nil
+}
+
+func (u User) scanRow(s pgx.Row) (model.User, error) {
+	m := model.User
+}
+
+
