9fa1bc598e3f900869fdaa2a2491f9d8a9bcb609
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-06 18:17:08 -0400

    Edición de handler con API struct

--- infrastructure/handler/user/handler.go
+++ infrastructure/handler/user/handler.go
@@ -2,17 +2,18 @@ package user
 
 import (
 	"e-commerce/domain/user"
+	"e-commerce/infrastructure/handler/response"
 	"e-commerce/model"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 type handler struct {
 	useCase user.UserCase
+	responsed response.API
 }
 
-// NewHandler retorna la implementación del dominio como un punto de entrada   
+// NewHandler retorna la implementación del dominio como un punto de entrada
 func newHandler(uc user.UserCase) handler {
 	return handler{useCase: uc}
 }
@@ -23,16 +24,24 @@ func (h *handler) Create(c echo.Context) (err error) {
 
 	//Si model.User == error, informar al cliente de la petición erronea con un .JSON del error
 	if err := c.Bind(&m); err != nil {
-		c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+		return h.responsed.BindFailed(err)
+		// return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
-	return c.JSON(http.StatusOK, m)
+
+	//Devolución de Error estandar.
+	if err := h.useCase.Create(&m); err != nil {
+		return h.responsed.Error(c, "useCase.Create()", err)
+		// return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(h.responsed.Created(m))
 }
 
 //Obtiene la información completa del userCase
 func (h *handler) GetAll(c echo.Context) (err error) {
 	users, err := h.useCase.GetAll()
 	if err != nil {
-		c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+		return h.responsed.Error(c, "useCase.GetAll()", err)
+		// c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
-	return c.JSON(http.StatusOK, users)
+	return c.JSON(h.responsed.OK(users))
 }
