d81c9466a492dcec36b677e1e9c759c71165d762
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-08 22:44:27 -0400

    Selecciona tabla con BuildSQLSelect

--- infrastructure/postgres/user/postgreSQL.go
+++ infrastructure/postgres/user/postgreSQL.go
@@ -17,11 +17,13 @@ func BuildSQLInsert(table string, fields []string) string {
 	values := bytes.Buffer{}
 	k:= 0
 
+	//Se itera cada campo, rellenando nombre, separación por coma (",") y un valor
 	for _, v := range fields {
 		k++
 		args.WriteString(v)
 		args.WriteString( ", ")
 		values.WriteString(fmt.Sprintf("$#{k},"))
+		values.WriteString(fmt.Sprintf("$%d, ", k))
 	}
 
 	args.Truncate(args.Len() - 2)
@@ -52,6 +54,34 @@ func BuildSQLUpdate(table string, fields []string) string {
 	return fmt.Sprintf("UPDATE %s SET %s WHERE id = $%d", table, args.String(), k)
 }
 
+//BuildSQLDelete elimina tabla del registro por ID
 func BuildSQLDelete(table string) string {
 	return fmt.Sprintf("DELETE FROM %s WHERE id = $1", table)
+}
+
+//BuildSQLSelect selecciona el campo dentro de la tabla del registro
+func BuildSQLSelect(table string, fields []string) string {
+	if len(fields) == 0 {
+		return FieldEmpty
+	}
+
+	args := bytes.Buffer{}
+	for _, v := range fields {
+		args.WriteString(fmt.Sprintf("%s, ", v))
+	}
+	args.Truncate(args.Len() - 2)
+
+	return fmt.Sprintf("SELECT %s FROM %s", args.String(), table)
+}
+
+//Int64Null rellena campos no válidos
+func Int64ToNull(d int64) sql.NullInt64 {
+	r := sql.NullInt64{Int64: d}
+	if d > 0 {
+		r.Valid = true
+	}
+
+	return r
+}
+
 }
