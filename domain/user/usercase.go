a238b117d34adcd6286729530a2da78fb32d3756
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-09 19:57:56 -0400

    Fix de Usercase de User (#19)

--- domain/user/usercase.go
+++ domain/user/usercase.go
@@ -5,30 +5,32 @@ import (
 	"time"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
+	"fmt"
 )
 
+//User representa la Storage
 type User struct {
 	storage Storage
 }
 
+//New crea un nuevo User, en el cual se almacena el storage de User
 func New(s Storage) User {
 	return User{storage: s}
 }
 
 //Creación de nuevo Usuario
-func (u User) Create(m *model.User) {
+func (u User) Create(m *model.User) error{
 	ID, err := uuid.NewUUID()
 	if err != nil {
-		return
-		//fmt.Printf("%s %w", "uuid.NewUUID()", err) 
+		return fmt.Errorf("%s %w", "uuid.NewUUID()", err)
 	}
 
+	//Veriificación de ID
 	m.ID = ID
 	//Cifrado de password
 	password, err := bcrypt.GenerateFromPassword([]byte(m.Password), bcrypt.DefaultCost)
 	if err != nil {
-		return
-		//fmt.Printf("%s %w", "bcrypt.GenerateFromPassword()", err)
+		return fmt.Errorf("%s %w", "bcrypt.GenerateFromPassword()", err)
 	}
 
 	m.Password = string(password)
@@ -39,19 +41,18 @@ func (u User) Create(m *model.User) {
 
 	err = u.storage.Create(m)
 	if err != nil {
-		return
-		//fmt.Printf("%s %w", "storage Create()", err) 
+		return fmt.Errorf("%s %w", "storage.Create()", err)
 	}
 
 	m.Password = ""
-	//return nil
+	return nil
 }
 
-//GetByEmail recibe como parametro un email. Obteniendo a traves de este parametro, la información contenida en el storage de User.
+//GetByEmail recibe como parametro un email. Obteniendo a través de este parametro, la información contenida en el storage de User.
 func (u User) GetByEmail(email string) (model.User, error) {
 	user, err := u.storage.GetByEmail(email)
 	if err != nil {
-		return model.User{}, err
+		return model.User{}, fmt.Errorf("%s %w", "storage.GetByEmail()", err)
 	}
 	return user, nil
 }
@@ -60,7 +61,7 @@ func (u User) GetByEmail(email string) (model.User, error) {
 func (u User) GetAll() (model.Users, error) {
 	users, err := u.storage.GetAll()
 	if err != nil {
-		return model.Users{}, err
+		return nil, fmt.Errorf("%s %w", "storage.GetAll()", err)
 	}
 	return users, nil
 }
