73b0dd0850ab17f74720b4cc6a022aa4266cfde6
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-10 19:19:44 -0400

    Update Handler del model.Product

--- infrastructure/handler/product/handler.go
+++ infrastructure/handler/product/handler.go
@@ -5,6 +5,7 @@ import (
 	"e-commerce/infrastructure/handler/response"
 	"e-commerce/model"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
@@ -32,4 +33,25 @@ func (h handler) Create(c echo.Context) error {
 	}
 
 	return c.JSON(h.response.Created(m))
+}
+
+// Update handler, maneja la actualización del model.Product
+func (h handler) Update(c echo.Context) error {
+	m := model.Product{}
+
+	if err := c.Bind(&m); err != nil {
+		return h.response.BindFailed(err)
+	}
+
+	ID, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		return h.response.BindFailed(err)
+	}
+	m.ID = ID
+
+	if err := h.useCase.Update(&m); err != nil {
+		return h.response.Error(c, "useCase.Update()", err)
+	}
+
+	return c.JSON(h.response.Updated(m))
 }
