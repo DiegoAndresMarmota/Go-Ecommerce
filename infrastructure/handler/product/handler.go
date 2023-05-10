aefc31118b17cb7b21664200add79bb9f7879ce3
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-10 19:23:25 -0400

    Get[ ] Handler model.Products

--- infrastructure/handler/product/handler.go
+++ infrastructure/handler/product/handler.go
@@ -56,7 +56,7 @@ func (h handler) Update(c echo.Context) error {
 	return c.JSON(h.response.Updated(m))
 }
 
-// Delete handles the deleting of a model.Product
+// Delete handler, maneja la eliminación del model.Product
 func (h handler) Delete(c echo.Context) error {
 	ID, err := uuid.Parse(c.Param("id"))
 	if err != nil {
@@ -69,4 +69,29 @@ func (h handler) Delete(c echo.Context) error {
 	}
 
 	return c.JSON(h.response.Deleted(nil))
+}
+
+//GetByID Handler, maneja le obtención de ID del model.Product
+func (h handler) GetByID(c echo.Context) error {
+	ID, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		return h.response.Error(c, "uuid.Parse()", err)
+	}
+
+	productData, err := h.useCase.GetByID(ID)
+	if err != nil {
+		return h.response.Error(c, "useCase.GetWhere()", err)
+	}
+
+	return c.JSON(h.response.OK(productData))
+}
+
+// GetAll Handler, maneja la busqueda completa de todos los model.Product
+func (h handler) GetAll(c echo.Context) error {
+	products, err := h.useCase.GetAll()
+	if err != nil {
+		return h.response.Error(c, "useCase.GetAllWhere()", err)
+	}
+
+	return c.JSON(h.response.OK(products))
 }
