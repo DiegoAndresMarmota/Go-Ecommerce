e501c27b8756ca298f283e5782782f1f7219cd7a
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-10 18:52:08 -0400

    Creación del handler model.Product

--- infrastructure/handler/product/handler.go
+++ infrastructure/handler/product/handler.go
@@ -3,6 +3,9 @@ package product
 import (
 	"e-commerce/domain/product"
 	"e-commerce/infrastructure/handler/response"
+	"e-commerce/model"
+
+	"github.com/labstack/echo/v4"
 )
 
 //
@@ -16,3 +19,17 @@ func newHandler(useCase product.UserCase) handler {
 	return handler{useCase: useCase}
 }
 
+//Create Handler, maneja la creacion de model.Product
+func (h handler) Create(c echo.Context) error {
+	m := model.Product{}
+
+	if err := c.Bind(&m); err != nil {
+		return h.response.BindFailed(err)
+	}
+
+	if err := h.useCase.Create(&m); err != nil {
+		h.response.Error(c, "useCase.Create", err)
+	}
+
+	return c.JSON(h.response.Created(m))
+}
