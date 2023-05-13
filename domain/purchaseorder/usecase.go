8903be708776f301e89625d6936d1cb32688000f
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-12 21:16:59 -0400

    Creación del struct de PurchaseOrder en useCase

--- domain/purchaseorder/usecase.go
+++ domain/purchaseorder/usecase.go
@@ -1,1 +1,15 @@
 package purchaseorder
+
+import (
+
+)
+
+// PurchaseOrder implementara los métodos del Storage
+type PurchaseOrder struct {
+	storage Storage
+}
+
+// New realiza a nueva PurchaseOrder, según los métodos del Storage
+func New(s Storage) PurchaseOrder {
+	return PurchaseOrder{storage: s}
+}
