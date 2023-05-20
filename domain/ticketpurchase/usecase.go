7a68c58ac645dd3596b5f75f8455f8b1edc4ec1c
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-05-20 19:25:59 -0400

    Features/domain ticketpurchase 01 (#30)
    * Creación SQL ticket_purchase
    
    * Creación SQL ticket_purchase_details
    
    * Creación del model de ticketPurchase
    
    * Creación de model de TicketPurchaseDetail
    
    * Creación de domain TicketPurchase
    
    * Creación de TicketPurchase y TicketPurchaseDetails

--- domain/ticketpurchase/usecase.go
+++ domain/ticketpurchase/usecase.go
@@ -2,7 +2,11 @@ package ticketpurchase
 
 import (
 	"e-commerce/model"
+	"encoding/json"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 // TicketPurchase implements UseCase
@@ -24,12 +28,12 @@ func (i TicketPurchase) Create(po *model.PurchaseOrder) error {
 	}
 
 	//Rellenar datos dentro de TicketPurchase && TicketPurchaseDetails en TicketPurchaseFromPurchaseOrder
-	TicketPurchase, TicketPurchaseDetails, err := TicketPurchaseFromPurchaseOrder(po)
+	ticketPurchase, ticketPurchaseDetails, err := ticketPurchaseFromPurchaseOrder(po)
 	if err != nil {
 		return fmt.Errorf("%s %w", "TicketPurchaseFromPurchaseOrder()", err)
 	}
 
-	err = i.storage.Create(&TicketPurchase, TicketPurchaseDetails)
+	err = i.storage.Create(&ticketPurchase, ticketPurchaseDetails)
 	if err != nil {
 		return fmt.Errorf("%s %w", "storage.Create()", err)
 	}
@@ -37,5 +41,46 @@ func (i TicketPurchase) Create(po *model.PurchaseOrder) error {
 	return nil
 }
 
+//TicketPurchaseFromPurchaseOrder recibe una orden de compra y devuelve un ticket de la compra y el detalle del mismo, además de un error.
+func ticketPurchaseFromPurchaseOrder(po *model.PurchaseOrder) (model.TicketPurchase, model.TicketPurchaseDetails, error) {
+	ID, err := uuid.NewUUID()
+	if err!= nil {
+			return model.TicketPurchase{}, nil, fmt.Errorf("%s %w", "uuid.NewUUID()", err)
+		}
 
-//Pendiente TicketPurchaseFromPurchaseOrder
+	//Creación del ticket de compra
+	ticketPurchase := model.TicketPurchase{
+		ID: ID,
+		UserID: po.UserID,
+		PurchaseOrderID: po.ID,
+		CreatedAt: time.Now().Unix(),
+	}
+
+	//Creación del detalle del ticket de compra
+	var products model.ProductToPurchases
+	err = json.Unmarshal(po.Products, &products)
+	if err != nil {
+		return model.TicketPurchase{}, nil, fmt.Errorf("%s %w", "json.Unmarshal()", err)
+	}
+
+	var ticketPurchaseDetails model.TicketPurchaseDetails
+	for _, v := range products {
+		detailID, err := uuid.NewUUID()
+		if err!= nil {
+					return model.TicketPurchase{}, nil, fmt.Errorf("%s %w", "json.Unmarshal()", err)
+				}
+
+		detail := model.TicketPurchaseDetail{
+			ID: detailID,
+			TicketPurchaseID: ticketPurchase.ID,
+			ProductID: v.ProductID,
+			Amount: v.Amount,
+			UnitPrice: v.UnitPrice,
+			CreatedAt: time.Now().Unix(),
+		}
+
+		ticketPurchaseDetails = append(ticketPurchaseDetails, detail)
+	}
+
+	return ticketPurchase, ticketPurchaseDetails, nil
+}
