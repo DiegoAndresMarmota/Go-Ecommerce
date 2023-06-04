89c2edb1229ef78fea6d211ef15117ea32701f6b
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-06-03 23:10:19 -0400

    Hotfix/database 02 (#37)
    * Creación de Login(email y password)
    
    * Creación del token firmado y validado
    
    * Creación del Handler de Login
    
    * Creación del route del handler
    
    * HotFix de paramsUrl
    
    * Fix camelCase paramsURL

--- cmd/database.go
+++ cmd/database.go
@@ -9,31 +9,30 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-const AppCommerce = "Go-ECommerce"
+const AppECommerce = "Go-ECommerce"
 
 //newDBConnection permite conectar a través de pgxpool con PostgreSQL
 func newDBConnection() (*pgxpool.Pool, error) {
-	min := 3
+	min := 5
 	max := 100
 
-	minConn := os.Setenv("DB_MIN_CONN")
-	maxConn := os.Setenv("DB_MAX_CONN")
-
 	//Variables de entorno
-	user := os.Setenv("DB_USER")
-	pass := os.Setenv("DB_PASSWORD")
-	host := os.Setenv("DB_HOST")
-	port := os.Setenv("DB_PORT")
-	dbName := os.Setenv("DB_NAME")
-	sslMode := os.Setenv("DB_SSL_MODE")
+	minConn := os.Getenv("DB_MIN_CONN")
+	maxConn := os.Getenv("DB_MAX_CONN")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	sslMode := os.Getenv("DB_SSL_MODE")
 
-	//Validación min && max
+	//Validación
 	if minConn != "" {
 		v, err := strconv.Atoi(minConn)
 		if err != nil {
-			log.Printf("Error: DB_MIN_CONN has not a valid value", os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT"), os.Getenv("DB_NAME") + ":" + os.Getenv("DB_SSL_MODE"))
+			log.Println("Warning: DB_MIN_CONN has not a valid value, we will set min connections to", min)
 		} else {
-			if v >= 3 && v <= 100 {
+			if v >= min && v <= max {
 				min = v
 			}
 		}
@@ -41,19 +40,33 @@ func newDBConnection() (*pgxpool.Pool, error) {
 	if maxConn != "" {
 		v, err := strconv.Atoi(maxConn)
 		if err != nil {
-			log.Printf("Error: DB_MAX_CONN has not a valid value", max)
+			log.Println("Warning: DB_MAX_CONN has not a valid value, we will set max connections to", max)
 		} else {
-			if v >= 3 && v <= 100 {
+			if v >= min && v <= max {
 				max = v
 			}
 		}
 	}
 
-//[] de conexión
-func makeConnection (
-	user, pass, host, port, dbName, sslMode string, minConn, maxConn int
-	) string {
-		return fmt.Sprintf("user=$s password=$s host=$s port=$s dbname=$s sslmode=$s pool_min_conns=$d pool_max_conns=%d",
+	//checkStatusString verifica y valida la conexión
+	checkStatusString := paramsURL(user, pass, host, port, dbName, sslMode, min, max)
+	config, err := pgxpool.ParseConfig(checkStatusString)
+	if err != nil {
+		return nil, fmt.Errorf("%s %w", "pgxpool.ParseConfig()", err)
+	}
+
+	config.ConnConfig.RuntimeParams["application_name"] = AppECommerce
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		return nil, fmt.Errorf("%s %w", "pgxpool.NewWithConfig()", err)
+	}
+
+	return pool, nil
+}
+
+func paramsURL(user, pass, host, port, dbName, sslMode string, minConn, maxConn int) string {
+	return fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s pool_min_conns=%d pool_max_conns=%d",
 		user,
 		pass,
 		host,
@@ -64,20 +77,3 @@ func makeConnection (
 		maxConn,
 	)
 }
-
-	//connPrincipal, verifica y valida la conexión
-	connPrincipal := makeConnection(user, pass, host, port, dbName, sslMode, min, max)
-	config, err := pgxpool.ParseConfig(connPrincipal)
-	if err != nil {
-		nil, fmt.Errorf("error parsing config: %w", err, "%s", "pgxpool.ParseConfig()")
-	}
-
-	config.ConnConfig.RuntimeParams["aplication_name"] = AppCommerce
-
-	pool, err := pgxpool.NewWithConfig(context.Background(), config)
-	if err != nil {
-		nil, fmt.Errorf("error parsing config: %w", err, "%s", "pgxpool.NewWithConfig()")
-	}
-	return pool, nil
-}
-
